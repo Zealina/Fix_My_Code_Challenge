22211763e5f90c56f43223145375e7fd2084d7dd
Sebastine Hamilton <hamilsebastine@gmail.com>
2023-12-19 09:14:00 +0000

    Define Square Class

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -2,7 +2,7 @@
 """Module for class square"""
 
 
-class square():
+class Square():
     """Square Class"""
 
     width = 0
@@ -28,7 +28,7 @@ class square():
 
 if __name__ == "__main__":
     """Create a square Object"""
-    s = square(width=12, height=9)
+    s = Square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
     print(s.PermiterOfMySquare())
